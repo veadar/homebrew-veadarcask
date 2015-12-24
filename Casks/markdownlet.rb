1c3683a9e9aa9288fde61c8da73d70525ee978a9
veadar <veadar@gmail.com>
2015-12-24 16:20:18 +0900

    add markdownlet

---
+++ Casks/markdownlet.rb
@@ -0,0 +1,7 @@
+cask :v1 => 'markdownlet' do
+  version '1.0.3'
+  url "https://github.com/veadar/markdownlet/releases/download/#{version}/markdownlet.app.zip"
+  homepage 'https://github.com/veadar/markdownlet'
+  sha256 'ad4f8233502f2a33838748d3b362436b7fcd6845697c11ddaeff292f2229b94a'
+  app 'markdownlet.app'
+end
