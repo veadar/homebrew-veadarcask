fdf1eebe2595468bd48f7143bae98f7e6f287b29
veadar <veadar@gmail.com>
2015-12-24 16:34:47 +0900

    add poorselect

---
+++ Casks/poorselect.rb
@@ -0,0 +1,7 @@
+cask :v1 => 'poorselect' do
+  version '0.2'
+  url "https://github.com/veadar/PoorSelect/releases/download/v#{version}/PoorSelect_ignore-case.app.zip"
+  homepage 'https://github.com/veadar/PoorSelect'
+  sha256 'c7e3f3300b9c9a9c0befa9854e4bf0bb6e9b73628e1118edd92e97734d371605'
+  app 'PoorSelect.app'
+end
