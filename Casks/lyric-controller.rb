b20a93e51348245427ac8a6852833ce50c94a25f
veadar <veadar@gmail.com>
2015-12-24 19:32:58 +0900

    add Lyric Controller

---
+++ Casks/lyric-controller.rb
@@ -0,0 +1,7 @@
+cask :v1 => 'lyric-controller' do
+  version '1'
+  url "https://github.com/veadar/Lyric-Controller/releases/download/v#{version}/Lyric.Controller.app.zip"
+  homepage 'https://github.com/veadar/Lyric-Controller'
+  sha256 '37b0f175292be194c320dc8dd9b87605a08a28dd52ab87e180cac6d73d89ba33'
+  app 'Lyric Controller.app'
+end
