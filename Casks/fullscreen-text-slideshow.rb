07e0aeac307d3c480bfe66bb743f9bf89babac09
veadar <veadar@gmail.com>
2015-12-24 14:15:01 +0900

    bug fix

--- Casks/fullscreen-text-slideshow.rb
+++ Casks/fullscreen-text-slideshow.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'fullscreen-text-slideshow' do
   version '1.1.2'
-  url 'https://github.com/veadar/fullscreen-text-slideshow/releases/download/v#{version}/fullscreen.text.slideshow.app.zip'
+  url "https://github.com/veadar/fullscreen-text-slideshow/releases/download/v#{version}/fullscreen.text.slideshow.app.zip"
   homepage 'https://github.com/veadar/fullscreen-text-slideshow'
   sha256 'cd907590527451239f0f131f9ec2e89d84d48d3fa49b5a4da3618046ff3516dc'
   link 'fullscreen text slideshow.app'
