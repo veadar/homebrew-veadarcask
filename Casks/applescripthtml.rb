3eb22693b1d4c77cb731adba54216c2ad85600e3
veadar <veadar@gmail.com>
2016-01-23 01:30:02 +0900

    add applescripthtml

---
+++ Casks/applescripthtml.rb
@@ -0,0 +1,7 @@
+cask 'applescripthtml' do
+  version '2.5.2'
+  url "http://www.script-factory.net/software/ScriptEditor/AppleScriptHTML/archives/AppleScriptHTML-2.5.2.dmg"
+  homepage 'http://www.script-factory.net/software/ScriptEditor/AppleScriptHTML/'
+  sha256 'caed3151e54f0f87580405ffe01319225651c7ce632acf84fec9bbecca8df98a'
+  app 'AppleScriptHTML.app'
+end
