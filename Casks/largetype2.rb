06a7f40a47ec5fcb6e72b92e5bdea7d3aa8beb3f
veadar <veadar@gmail.com>
2016-04-16 13:01:55 +0900

    add LargeType2

---
+++ Casks/largetype2.rb
@@ -0,0 +1,7 @@
+cask 'largetype2' do
+  version '1.0'
+  url "http://blyt.net/algrtepy2e/largetype2-100.dmg"
+  homepage 'http://blyt.net/algrtepy2e/'
+  sha256 'e47c31ffee67b618245baf2296d1e5007dfa0fdb72008b4f36f8dab0d1bb66fe'
+  app 'LargeType2.app'
+end
