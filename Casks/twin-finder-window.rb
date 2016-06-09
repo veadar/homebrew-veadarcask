d11bb9be53ac6ec1a382636e08e7124554b081ad
veadar <veadar@gmail.com>
2016-06-09 23:58:54 +0900

    add twin-finder-window

---
+++ Casks/twin-finder-window.rb
@@ -0,0 +1,7 @@
+cask 'twin-finder-window' do
+  version '0.1'
+  url "https://github.com/veadar/twin-finder-window/releases/download/v#{version}/twin.finder.window.app.zip"
+  homepage 'https://github.com/veadar/twin-finder-window'
+  sha256 '131e5cfd570291bb989f491f142091e84c1f5b65b608690114213e77e199321f'
+  app 'twin finder window.app'
+end
