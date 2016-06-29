1a354f8a0aae1da419702954254d76fec2049018
veadar <veadar@gmail.com>
2016-06-29 10:52:39 +0900

    update poorselect,add powerselect

--- Casks/poorselect.rb
+++ Casks/poorselect.rb
@@ -1,7 +1,7 @@
 cask 'poorselect' do
-  version '0.2'
+  version '0.3'
   url "https://github.com/veadar/PoorSelect/releases/download/v#{version}/PoorSelect_ignore-case.app.zip"
   homepage 'https://github.com/veadar/PoorSelect'
-  sha256 'c7e3f3300b9c9a9c0befa9854e4bf0bb6e9b73628e1118edd92e97734d371605'
+  sha256 '0b427f1e690b2650914c999aca24d6c31d1690178522ba4701997f01f77b9980'
   app 'PoorSelect.app'
 end
---
+++ Casks/powerselect.rb
@@ -0,0 +1,7 @@
+cask 'powerselect' do
+  version '2.5'
+  url "http://www.script-factory.net/software/FinderHelpers/PowerSelect/archives/PowerSelect-#{version}.dmg"
+  homepage 'http://www.script-factory.net/software/FinderHelpers/PowerSelect/'
+  sha256 'e7b80b9e9c920f6f8a05f0f4404d87d43026602cf5ef0dd562751cf688748690'
+  app 'PowerSelect.app'
+end
