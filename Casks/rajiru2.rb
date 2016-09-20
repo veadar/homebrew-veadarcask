ce100590d0025013191e02150fff8383aa18f3da
veadar <veadar@gmail.com>
2016-09-20 21:36:14 +0900

    add rajiru2

---
+++ Casks/rajiru2.rb
@@ -0,0 +1,7 @@
+cask 'rajiru2' do
+  version '1.0'
+  url "https://github.com/veadar/rajiru2/releases/download/v#{version}/rajiru2.app.zip"
+  homepage 'https://github.com/veadar/rajiru2'
+  sha256 'a894eb99fe414a0e803d1d31073b4837d78e15b8278befa0a62b20cf706f3db2'
+  app 'rajiru2.app'
+end
