b0a7c9e2c25d3cd67697add6fb555ac096da9329
veadar <veadar@gmail.com>
2016-09-24 15:33:24 +0900

    add rajiru2 kindlestrip-kai

---
+++ Casks/kindlestrip-kai.rb
@@ -0,0 +1,7 @@
+cask 'kindlestrip-kai' do
+  version '1.0'
+  url "https://github.com/veadar//KindleStrip-Kai/releases/download/v#{version}/kindlestrip_kai.app.zip"
+  homepage 'https://github.com/veadar/KindleStrip-Kai/releases'
+  sha256 '36fc46187704e2ff57a4ce915cbd677471f72d421d34fcf390c39699441f75ff'
+  app 'kindlestrip_kai.app'
+end
--- Casks/rajiru2.rb
+++ Casks/rajiru2.rb
@@ -1,7 +1,7 @@
 cask 'rajiru2' do
-  version '1.0'
+  version '1.2'
   url "https://github.com/veadar/rajiru2/releases/download/v#{version}/rajiru2.app.zip"
   homepage 'https://github.com/veadar/rajiru2'
-  sha256 'a894eb99fe414a0e803d1d31073b4837d78e15b8278befa0a62b20cf706f3db2'
+  sha256 '48fec4cd15cd5a1506014afb2e9abf4d22c55c867bed96f3588577a66c6a3af9'
   app 'rajiru2.app'
 end
