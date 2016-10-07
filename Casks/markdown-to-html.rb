2590adcd16fbe3ca954ed35a534a98342553ad60
veadar <veadar@gmail.com>
2016-10-07 23:13:57 +0900

    add Markdown to HTML

---
+++ Casks/markdown-to-html.rb
@@ -0,0 +1,7 @@
+cask 'markdown-to-html' do
+  version '1.0'
+  url "https://github.com/veadar/Markdown-to-HTML/releases/download/v#{version}/Markdown.to.HTML.app.zip"
+  homepage 'https://github.com/veadar/Markdown-to-HTML/'
+  sha256 '12b81cd2e6128a72875b5a7cd60fbd83fd8aa7fb850a12931d7eedd55e947de4'
+  app 'Markdown to HTML.app'
+end
