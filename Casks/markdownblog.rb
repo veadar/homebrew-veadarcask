1e29cbd2a6898f20b924728b5522a73950689c50
veadar <veadar@gmail.com>
2016-10-15 16:13:52 +0900

    add markdownblog

---
+++ Casks/markdownblog.rb
@@ -0,0 +1,7 @@
+cask 'markdownblog' do
+  version '1.0'
+  url "https://github.com/veadar/markdownBlog/releases/download/v#{version}/markdownBlog.app.zip"
+  homepage 'https://github.com/veadar/markdownBlog/'
+  sha256 '31844076926a04f790d565c71faa9ed8915c02298d6e75ad8b7436a890c3e668'
+  app 'markdownBlog.app'
+end
