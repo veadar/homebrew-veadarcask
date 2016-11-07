91ed970a21762461718b4fb9f689c1b781290fae
veadar <veadar@gmail.com>
2016-11-07 10:31:40 +0900

    fix

--- Casks/markdownblog.rb
+++ Casks/markdownblog.rb
@@ -1,7 +1,7 @@
 cask 'markdownblog' do
-  version '1.1.2'
+  version '1.1.3'
   url "https://github.com/veadar/markdownBlog/releases/download/v#{version}/markdownBlog.app.zip"
   homepage 'https://github.com/veadar/markdownBlog/'
-  sha256 'ddfc51af4958afa145e76e3100270c5cd3bbe50249d53a3df1ede4a80421de1e'
+  sha256 '90280a2ac2e53edd77d585e506fca33fc3733415c4545413158829393608bdb7'
   app 'markdownBlog.app'
 end
