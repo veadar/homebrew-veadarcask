9ca0e6fe63498683fd9a1d3d3c06bce756890e12
veadar <veadar@gmail.com>
2017-03-01 12:40:10 +0900

    add iina and sands

---
+++ Casks/iina.rb
@@ -0,0 +1,7 @@
+cask 'iina' do
+  version '0.0.6'
+  url "https://github.com/lhc70000/iina/releases/download/v#{version}/IINA.v#{version}.dmg"
+  homepage 'https://lhc70000.github.io/iina/'
+  sha256 '1395d8e5574dce5e7fa5685a66403259501463cc09abd1c47b653ccfdd487015'
+  app 'IINA.app'
+end
---
+++ Casks/sands.rb
@@ -0,0 +1,7 @@
+cask 'sands' do
+  version '1.0'
+  url "https://github.com/ToQoz/SandS/releases/download/v#{version}/SandS.app.zip"
+  homepage 'https://github.com/ToQoz/SandS'
+  sha256 'c6d159b81a4c21110c8b86e373163cdfcaff6af8a5d79ded10481d430d956089'
+  app 'SandS.app'
+end
