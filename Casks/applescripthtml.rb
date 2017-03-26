66a051701384c7880ed9da8f684f82f4c3bf7140
veadar <veadar@gmail.com>
2017-03-26 21:03:04 +0900

    version up applescripthtml

--- Casks/applescripthtml.rb
+++ Casks/applescripthtml.rb
@@ -1,7 +1,7 @@
 cask 'applescripthtml' do
-  version '2.5.2'
-  url "http://www.script-factory.net/software/ScriptEditor/AppleScriptHTML/archives/AppleScriptHTML-2.5.2.dmg"
+  version '2.7.2'
+  url "http://www.script-factory.net/software/ScriptEditor/AppleScriptHTML/archives/AppleScriptHTML-#{version}.dmg"
   homepage 'http://www.script-factory.net/software/ScriptEditor/AppleScriptHTML/'
-  sha256 'caed3151e54f0f87580405ffe01319225651c7ce632acf84fec9bbecca8df98a'
+  sha256 'd716edfbf08d522509ba48419d35a4862084faaca08bed8d836e58958eca3e53'
   app 'AppleScriptHTML.app'
 end
