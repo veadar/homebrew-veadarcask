ba5f5d4e2c892f42192e87997f2d83d1856bea21
veadar <veadar@gmail.com>
2017-12-05 01:13:25 +0900

    change rajiru

--- Casks/rajiru2.rb
+++ Casks/rajiru2.rb
@@ -1,7 +1,7 @@
 cask 'rajiru2' do
-  version '1.5.3'
+  version '1.5.4'
   url "https://github.com/veadar/rajiru2/releases/download/v#{version}/rajiru2.app.zip"
   homepage 'https://github.com/veadar/rajiru2'
-  sha256 'f5ce030b2ed13a7aba51592da5ab146826910ccd6e8d99595dfb604536fa8ba3'
+  sha256 '03fbcbc606eb6ab3bf56d2d404bd6baf0aa10a1f53d3de29f922064462a59c1f'
   app 'rajiru2.app'
 end
