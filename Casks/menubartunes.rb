224bf63fd324c2ecd1965f8a3bf2bf11c627d1e9
veadar <veadar@gmail.com>
2019-02-26 22:36:04 +0900

    bug fix sha256

--- Casks/menubartunes.rb
+++ Casks/menubartunes.rb
@@ -2,6 +2,6 @@ cask 'menubartunes' do
   version '1.1'
   url "https://github.com/veadar/menubarTunes/releases/download/v#{version}/menubarTunes.app.zip"
   homepage 'https://github.com/veadar/menubarTunes'
-  sha256 '7f264e45a837ede8b81194b71c83add5ba26c3ab5ca9c90e3d6ae188f3b8aa0d'
+  sha256 '0be5a3e99d843eee38ee1672e7f5ca0d7d98da021d19b2214917d739a15d8b8d'
   app 'menubarTunes.app'
 end
