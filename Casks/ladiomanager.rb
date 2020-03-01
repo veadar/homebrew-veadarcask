1ab06e1c076aacd8dc378a142190d323380eca74
veadar <veadar@gmail.com>
2020-03-01 11:30:16 +0900

    ladiomanagerをアップデート

--- Casks/ladiomanager.rb
+++ Casks/ladiomanager.rb
@@ -1,7 +1,8 @@
 cask 'ladiomanager' do
   version '0.0.17'
-  url "http://gien.nm.land.to/LadioManager/download.php?file=LadioManager-#{version}.dmg"
-  homepage 'http://gien.nm.land.to/LadioManager/'
   sha256 '578c53e490c2fdea4b1c200f144c717f8943f148417edf17f57bf533127cfa4c'
+  url "http://gien.nm.land.to/LadioManager/download.php?file=LadioManager-0.0.17.dmg"
+  name 'LadioManager'
+  homepage ''
   app 'LadioManager.app'
 end
